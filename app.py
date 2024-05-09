1f21b977ad0a291769ef25f0c24ab4c62beb0654
MaximilianoTauil <maxitauil@gmail.com>
2024-05-08 21:41:07 -0300

    Actualizar 8/5

--- app.py
+++ app.py
@@ -2,10 +2,16 @@ import streamlit as st
 import pandas as pd
 
 data = pd.read_parquet('Web_scrapp.parquet')
+data1 = pd.read_parquet('Titulos.parquet')
+data2 = pd.read_parquet('Letras.parquet')
 
 def main():
     st.title('Acciones Líderes')
     st.write(data)
+    st.title('Títulos')
+    st.write(data1)
+    st.title('Letras')
+    st.write(data2)
 
     
 if __name__ == "__main__":
